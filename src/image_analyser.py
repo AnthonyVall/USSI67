8334ef780bb8f14231fdedb10f7533d8c3bec7ea
Anthony Vallon <anthony.vallon.dev@gmail.com>
2026-02-04 12:12:40 +0100

    push updates

--- src/image_analyser.py
+++ src/image_analyser.py
@@ -1,4 +1,4 @@
-from utils import render_image, save_image, transform_frame_to_array
+from utils import render_image, save_image, transform_frame_to_array, compute_pixel_size
 from pymba import Vimba, VimbaException, Frame
 from pyzbar.pyzbar import decode, ZBarSymbol
 from keras.models import load_model
@@ -11,6 +11,7 @@ import cv2
 
 
 running = False
+pixel_size = -1
 
 
 def run(camera: int = 0,
@@ -45,6 +46,9 @@ def run(camera: int = 0,
             return_img = cv2.polylines(img, [np.array([code.polygon], np.int32)], True, (0, 255, 0), 3)
             bielletteSaver.add_biellette(code.data, img)
 
+
+
+
         return return_img
 
 
@@ -67,11 +71,17 @@ def run(camera: int = 0,
             bielletteSaver.save_biellettes()
             global running
             running = False
+            cv2.destroyAllWindows()
 
 
     with Vimba() as vimba:
         camera = vimba.camera(camera)
         camera.open()
+
+        camera.arm('SingleFrame')
+        global pixel_size
+        pixel_size = compute_pixel_size(transform_frame_to_array(camera.acquire_frame(), size=size), rect_size = (170, 120))
+        camera.disarm()
 
         camera.arm('Continuous', handle_frames)
         camera.start_frame_acquisition()
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,10 @@
+from sklearn.cluster import KMeans
 from typing import Tuple
 from pathlib import Path
 from pymba import Frame
+import pandas as pd
 import numpy as np
+import math
 import cv2
 
 PIXEL_FORMATS_CONVERSIONS = {
@@ -26,3 +29,51 @@ def render_image(img: np.array, window_name: str = "Window") -> None:
 
 def save_image(img: np.array, path: Path) -> None:
     cv2.imwrite(str(path), img)
+
+
+def compute_pixel_size(image: np.array, rect_size: Tuple[float, float]) -> Tuple[float, float]:
+    img = cv2.Canny(image, 200, 230)
+    lines = cv2.HoughLinesP(img, rho=1, theta=np.pi / 180, threshold=100, minLineLength=250, maxLineGap=100)
+    lines = [[line[0][0], line[0][1], line[0][2], line[0][3], math.degrees(math.atan2(line[0][3] - line[0][1], line[0][2] - line[0][0]))]  for line in lines]
+
+    # TODO have angle more important ?
+    data: pd.DataFrame = pd.DataFrame(lines, columns=["x1", "y1", "x2", "y2", "angle"])
+    init = np.array([
+        [540, 0, 540, 0, -90],
+        [540, 920, 540, 920, -90],
+        [0, 460, 0, 460, 0],
+        [1080, 460, 1080, 460, 0]
+    ])
+    kmeans = KMeans(n_clusters=4, init=init)
+    kmeans.fit(data)
+
+    l = dict()
+    data['group'] = kmeans.labels_
+    for label in range(4):
+        d = data[data['group'] == label]
+        l[label] = ((min(d['x1']), min(d['y1'])), (max(d['x2']), max(d['y2'])))
+
+    print(l)
+    return ""
+
+
+if "__main__" == __name__:
+    img = cv2.imread('../data/square.png')
+
+    dst = np.copy(img)
+    print(compute_pixel_size(img, rect_size=(170, 120)))
+
+    """
+    img  = cv2.Canny(img, 200, 230)
+    lines = cv2.HoughLinesP(img, rho=1, theta=np.pi/180, threshold=100, minLineLength=250, maxLineGap=100)
+
+    lines: list[Tuple[Tuple[int, int], Tuple[int, int]]] = [((line[0][0], line[0][1]), (line[0][2], line[0][3])) for line in lines]
+    for line in lines:
+        cv2.line(dst, line[0], line[1], (255, 0, 0), 2)
+
+    cv2.imshow('img', dst)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    """
+
+    # (2.911764705882353, 4.966666666666667)
